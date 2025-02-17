9fe39b519d93032bca06782c16a077135d4c3355
PONPISITH <167839093+Ponpisith@users.noreply.github.com>
2025-02-17 13:38:55 +0700

    upgrade serviceAPI

--- lib/serviceapi.dart
+++ lib/serviceapi.dart
@@ -1,77 +1,139 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 import 'package:serviceapi/constant/httpmethods.dart';
 
 class ServiceAPI {
-  final _dio = Dio();
+  late final Dio _dio;
   final String baseURL;
   final String port;
-  final String endPonint;
+  final String endpoint;
+  final Map<String, String>? headers; // Allow custom headers
 
-  ServiceAPI(
-      {required this.baseURL, required this.port, required this.endPonint});
+  ServiceAPI({
+    required this.baseURL,
+    required this.port,
+    required this.endpoint,
+    this.headers, // Optional headers
+  }) {
+    _dio = Dio();
+    _configureDio();
+  }
 
   String get _pathAPI => '$baseURL${port.isNotEmpty ? ':$port' : ''}/';
 
   void _configureDio() {
-    // Set default configs
-    _dio.options.baseUrl = _pathAPI;
-    _dio.options.connectTimeout = Duration(seconds: 5);
-    _dio.options.receiveTimeout = Duration(seconds: 3);
+    _dio.options = BaseOptions(
+      baseUrl: _pathAPI,
+      connectTimeout: const Duration(milliseconds: 10),
+      receiveTimeout: const Duration(milliseconds: 5),
+      sendTimeout: const Duration(milliseconds: 5),
+      contentType: 'application/json',
+      headers: {
+        'Accept': 'application/json',
+        ...?headers,
+      },
+    );
+
+    _dio.interceptors.add(LogInterceptor(
+      request: true,
+      requestBody: true,
+      responseBody: true,
+      error: true,
+    ));
+
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        debugPrint("Request to: ${options.uri}");
+        return handler.next(options);
+      },
+      onResponse: (response, handler) {
+        debugPrint("Response: ${response.statusCode}");
+        return handler.next(response);
+      },
+      onError: (DioException e, handler) {
+        debugPrint("Error: ${e.message}");
+        return handler.next(e);
+      },
+    ));
   }
 
-  Future<Response> request(String endPonint,
-      {Object? data,
-      Map<String, dynamic>? queryParameters,
-      CancelToken? cancelToken,
-      Options? options,
-      ProgressCallback? onSendProgress,
-      ProgressCallback? onReceiveProgress,
-      Httpmethods? httpMethod}) async {
-    if (httpMethod == null) {
-      throw Exception('Please enter httpMethod');
-    }
+  Future<Response> request(
+    String endpoint, {
+    Object? data,
+    Map<String, dynamic>? queryParameters,
+    CancelToken? cancelToken,
+    Options? options,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+    required Httpmethods httpMethod,
+    int retryCount = 0, // Retry count for failed requests
+  }) async {
     late Response response;
 
-    _configureDio();
-
-    switch (httpMethod) {
-      //GET
-      case Httpmethods.GET:
-        response = await _dio.get(endPonint,
-            data: data,
-            cancelToken: cancelToken,
+    try {
+      switch (httpMethod) {
+        case Httpmethods.GET:
+          response = await _dio.get(
+            endpoint,
             queryParameters: queryParameters,
-            onReceiveProgress: onReceiveProgress,
-            options: options);
-      //POST
-      case Httpmethods.POST:
-        response = await _dio.post(endPonint,
-            data: data,
             cancelToken: cancelToken,
-            queryParameters: queryParameters,
+            options: options,
             onReceiveProgress: onReceiveProgress,
+          );
+          break;
+        case Httpmethods.POST:
+          response = await _dio.post(
+            endpoint,
+            data: data,
+            queryParameters: queryParameters,
+            cancelToken: cancelToken,
+            options: options,
             onSendProgress: onSendProgress,
-            options: options);
-      //PUT
-      case Httpmethods.PUT:
-        response = await _dio.put(endPonint,
-            data: data,
-            cancelToken: cancelToken,
-            queryParameters: queryParameters,
             onReceiveProgress: onReceiveProgress,
-            onSendProgress: onSendProgress,
-            options: options);
-      //DELETE
-      case Httpmethods.DELETE:
-        response = await _dio.delete(endPonint,
+          );
+          break;
+        case Httpmethods.PUT:
+          response = await _dio.put(
+            endpoint,
             data: data,
-            cancelToken: cancelToken,
             queryParameters: queryParameters,
-            options: options);
+            cancelToken: cancelToken,
+            options: options,
+            onSendProgress: onSendProgress,
+            onReceiveProgress: onReceiveProgress,
+          );
+          break;
+        case Httpmethods.DELETE:
+          response = await _dio.delete(
+            endpoint,
+            data: data,
+            queryParameters: queryParameters,
+            cancelToken: cancelToken,
+            options: options,
+          );
+          break;
+      }
+    } on DioException catch (e) {
+      if (retryCount > 0) {
+        debugPrint("Retrying... attempts left: $retryCount");
+        return request(
+          endpoint,
+          data: data,
+          queryParameters: queryParameters,
+          cancelToken: cancelToken,
+          options: options,
+          onSendProgress: onSendProgress,
+          onReceiveProgress: onReceiveProgress,
+          httpMethod: httpMethod,
+          retryCount: retryCount - 1,
+        );
+      }
+      debugPrint("Request failed: ${e.message}");
+      rethrow;
     }
     return response;
   }
 
-  void showbaseURL() => print('BaseURL : $baseURL');
-  void showport() => print('Port : $port');
+  void showBaseURL() => debugPrint('BaseURL: $_pathAPI');
+  void showPort() => debugPrint('Port: $port');
 }
